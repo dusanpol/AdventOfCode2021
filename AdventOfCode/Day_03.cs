fc85129e16c3ff3845151e106f13c25df9681c9e
DiPi <dusan.polacek@outlook.sk>
2021-12-18 05:07:10 +0100

    day 3 task 1 solution

--- AdventOfCode/Day_03.cs
+++ AdventOfCode/Day_03.cs
@@ -11,11 +11,57 @@ public class Day_03 : BaseDay
 
     public override ValueTask<string> Solve_1()
     {
-        throw new NotImplementedException();
+        var lines = ParseLines();
+        int digitCount = lines[0].Length;
+        
+        // check input for incorrect digit count and that is binary
+        if (lines.Any(line => line.Length != digitCount || !line.All(ch => ch == '0' || ch == '1')))
+            throw new InvalidDataException();
+
+        int gamma = 0;
+        int epsilon = 0;
+
+        // iterate digits from most significant to least significant (in binary form!)
+        for (int d = 0; d < digitCount; d++)
+        {
+            int oneBitCount = lines.Where(l => l[d] == '1').Count();
+            int zeroBitCount = lines.Length - oneBitCount; // no need to iterate again
+
+            bool setGammaBit = oneBitCount > zeroBitCount; // set current gamma digit to 1, if it's the most common?
+            bool setEpsilonBit = !setGammaBit; // the least common epsilon bit is the opposite of gamma bit
+
+            // since we are iterating from most significant bit, we need index base: digitCount - 1
+            // and shift to current digit index
+            int digitIndex = digitCount - 1 - d;
+
+            if (setGammaBit)
+            {
+                int mask = 1 << digitIndex;
+                gamma |= mask;
+            }
+
+            if (setEpsilonBit)
+            {
+                int mask = 1 << digitIndex;
+                epsilon |= mask;
+            }
+        }
+
+        int consumption = gamma * epsilon;
+
+        var solution = consumption.ToString();
+        return ValueTask.FromResult(solution);
     }
 
     public override ValueTask<string> Solve_2()
     {
         throw new NotImplementedException();
+    }
+
+    private string[] ParseLines()
+    {
+        return _input
+            .Split('\n', StringSplitOptions.RemoveEmptyEntries)
+            .ToArray();
     }
 }
