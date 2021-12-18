779aa1c9bb1eea2c5d438ec4563950079340f169
DiPi <dusan.polacek@outlook.sk>
2021-12-18 05:19:38 +0100

    refactor

--- AdventOfCode/Day_03.cs
+++ AdventOfCode/Day_03.cs
@@ -2,37 +2,42 @@ namespace AdventOfCode;
 
 public class Day_03 : BaseDay
 {
-    private readonly string _input;
-
+    private readonly string[] _inputLines;
+    private readonly int _digitCount;
+    
     public Day_03()
     {
-        _input = File.ReadAllText(InputFilePath);
+        string[] inputLines = File
+            .ReadAllText(InputFilePath)
+            .Split('\n', StringSplitOptions.RemoveEmptyEntries)
+            .ToArray();
+
+        _digitCount = inputLines[0].Length;
+
+        // check input for incorrect digit count and that is binary
+        if (inputLines.Any(line => line.Length != _digitCount || !line.All(ch => ch == '0' || ch == '1')))
+            throw new InvalidDataException();
+
+        _inputLines = inputLines;
     }
 
     public override ValueTask<string> Solve_1()
     {
-        var lines = ParseLines();
-        int digitCount = lines[0].Length;
-        
-        // check input for incorrect digit count and that is binary
-        if (lines.Any(line => line.Length != digitCount || !line.All(ch => ch == '0' || ch == '1')))
-            throw new InvalidDataException();
-
         int gamma = 0;
         int epsilon = 0;
 
         // iterate digits from most significant to least significant (in binary form!)
-        for (int d = 0; d < digitCount; d++)
+        for (int d = 0; d < _digitCount; d++)
         {
-            int oneBitCount = lines.Where(l => l[d] == '1').Count();
-            int zeroBitCount = lines.Length - oneBitCount; // no need to iterate again
+            int oneBitCount = _inputLines.Where(l => l[d] == '1').Count();
+            int zeroBitCount = _inputLines.Length - oneBitCount; // no need to iterate again
 
             bool setGammaBit = oneBitCount > zeroBitCount; // set current gamma digit to 1, if it's the most common?
             bool setEpsilonBit = !setGammaBit; // the least common epsilon bit is the opposite of gamma bit
 
             // since we are iterating from most significant bit, we need index base: digitCount - 1
             // and shift to current digit index
-            int digitIndex = digitCount - 1 - d;
+            int digitIndex = _digitCount - 1 - d;
 
             if (setGammaBit)
             {
@@ -56,12 +61,5 @@ public class Day_03 : BaseDay
     public override ValueTask<string> Solve_2()
     {
         throw new NotImplementedException();
-    }
-
-    private string[] ParseLines()
-    {
-        return _input
-            .Split('\n', StringSplitOptions.RemoveEmptyEntries)
-            .ToArray();
     }
 }
