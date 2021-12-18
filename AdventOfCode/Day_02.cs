910f2e93308b33d8ecb15c041cf55de0b44dd144
DiPi <dusan.polacek@outlook.sk>
2021-12-18 02:38:43 +0100

    scaffolded day 2 class

---
+++ AdventOfCode/Day_02.cs
@@ -0,0 +1,22 @@
+namespace AdventOfCode
+{
+    public class Day_02 : BaseDay
+    {
+        private readonly string _input;
+
+        public Day_02()
+        {
+            _input = File.ReadAllText(InputFilePath);
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
