799179c84199974e82875c86f4b445e149854fa9
DiPi <dusan.polacek@outlook.sk>
2021-12-18 06:29:05 +0100

    scaffolded day 4 class

---
+++ AdventOfCode/Day_04.cs
@@ -0,0 +1,18 @@
+namespace AdventOfCode;
+
+public class Day_04 : BaseDay
+{
+    public Day_04()
+    {
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        throw new NotImplementedException();
+    }
+}
